771d2fc86a0697a6464d01cde3c6da4626b116fe
JuSuperSonic <julien.lepersonnic@protonmail.com>
2025-05-19 20:04:03 +0200

    Init terraform scructure.

---
+++ environments/dev/main.tf
@@ -0,0 +1,5 @@
+module "r2_bucket" {
+  source      = "../../modules/r2_bucket"
+  account_id  = var.cloudflare_account_id
+  bucket_name = var.r2_bucket_name
+}
---
+++ environments/dev/variables.tf
@@ -0,0 +1,11 @@
+variable "cloudflare_api_token" {
+  type = string
+}
+
+variable "cloudflare_account_id" {
+  type = string
+}
+
+variable "r2_bucket_name" {
+  type = string
+}
