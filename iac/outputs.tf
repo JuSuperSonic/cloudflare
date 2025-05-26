67dc3eba531e707f2b785573c20092eb601648a4
Julien Le Personnic <julien.le-personnic@capgemini.com>
2025-05-26 13:56:33 +0200

    Migrate Cloudflared resource into dedicated module.

--- iac/outputs.tf
+++ iac/outputs.tf
@@ -1,20 +1,38 @@
 output "account_id" {
   description = "Account Id."
-  value = var.account_id
+  value       = var.account_id
 }
 
-output "tunnel_cloudflared_name" {
-  description = "Cloudflared Tunnel Name."
-  value = cloudflare_zero_trust_tunnel_cloudflared.tunnel_cloudflared.name
+output "zone_id" {
+  description = "Zone Id."
+  value       = var.account_id
+}
+
+output "domain" {
+  description = "Domain."
+  value       = var.domain
+}
+
+output "dns_records" {
+  description = "DNS records."
+  value = {
+    for name, record in module.cloudflare_tunnel.dns_records :
+    format("%s.%s", name, var.domain) => record.content
+  }
 }
 
 output "tunnel_cloudflared_id" {
   description = "Cloudflared Access Id."
-  value = cloudflare_zero_trust_tunnel_cloudflared.tunnel_cloudflared.id
+  value       = module.cloudflare_tunnel.tunnel_cloudflared_id
+}
+
+output "tunnel_cloudflared_name" {
+  description = "Cloudflared Tunnel Name."
+  value       = module.cloudflare_tunnel.tunnel_cloudflared_name
 }
 
 output "tunnel_cloudflared_secret" {
   description = "Cloudflared Tunnel Secret."
-  value     = var.tunnel_cloudflared_secret
-  sensitive = true
+  value       = module.cloudflare_tunnel.tunnel_cloudflared_secret
+  sensitive   = true
 }
